638167463df33c1524291f97023e7fc6504bd15e
LanI <lani0001@gmail.com>
2023-06-05 14:21:43 +0800

    modified setup.py __init__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='devopsapi_module',
@@ -6,5 +6,4 @@ setup(
     install_requires=[
         'redis'
     ],
-    packages=find_packages()
 )
---
+++ src/devopsapi_module/__init__.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+from .redis import *
+from .logger import *
